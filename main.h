392517931f1993c978a94d6ba0ab3414600417c3
youssefnagib <youssefnagib@yahoo.com>
2023-07-27 02:39:44 +0200

    yalla

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void execmd(char **argv);
+char *get_location(char *command);
+
+#endif
